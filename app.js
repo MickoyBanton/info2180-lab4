2d4f4ba32d161d16c99444a62751f224a6afc0ae
Mickoy Banton <mickoybanton11@gmail.com>
2023-11-12 23:08:43 -0500

    Exercise 3 completed

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ window.addEventListener
 ("load", () =>
 {
     const form = document.querySelector("form");
+    const result=document.querySelector("#result");
+    const input=document.querySelector("input");
 
-    form.addEventListener('submit', function(event) {
-
-        event.preventDefault();
-
-        fetch("superheroes.php")
+    fetch(`superheroes.php`)
 
         .then(response => response.text())
 
         .then(data => 
         {
-            alert(data);
+            result.innerHTML = data;
+        })
+        .catch(error => 
+        {
+            console.log(error);
+        });
+
+    
+    
+    form.addEventListener('submit', function(event) {
+
+        event.preventDefault();
+        let heroname = input.value.trim();
+        let fetch_paramter = `superheroes.php?name=${heroname}`;
+
+        fetch(fetch_paramter)
+
+        .then(response => response.text())
+
+        .then(data => 
+        {
+            result.innerHTML = data;
         })
         .catch(error => 
         {
